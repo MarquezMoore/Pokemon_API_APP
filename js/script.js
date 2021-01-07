385c87ee45397edf62abb4677c843033b2b9fc17
MarquezMoore <marquezmoore23@gmail.com>
2021-01-07 17:37:55 +0100

    created list for pokedex
    This list will be used to store information about each pokemon within it.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // JavaScript Document
-alert("Hello World");
-
-let favoriteFood = "Steak";
-document.write(favoriteFood);
+let pokemonList = [
+    {name:'Bulbasaur', type:['grass','posion'], height: 0.7}, 
+    {name:'Charizard', type:['fire','flying'], height: 1.7}, 
+    {name:'Squirtle', type:['water'], height: 0.5}
+];

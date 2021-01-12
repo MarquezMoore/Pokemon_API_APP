63bbdf9e9d4babb4f8c01d032a71ce2c8ea2798d
MarquezMoore <marquezmoore23@gmail.com>
2021-01-12 20:28:14 +0100

    refacted code
    Condensed code into a more pure form.
    
    Nested global varibles in a IIFE.
    
    Created an add and getAll function within the IIFE to add and retrive pokemons.
    
    The IIFE's return statement returns an object with functions that are avaible inside IIFE. (Returning object is store in pokemonRepository)
    
    Used to forEach function of the pokemonRepository object to code functions within IIFE.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
 // JavaScript Document
 
 //this is my list of Pokemons
-let pokemonList = [
-    {name:'Bulbasaur', type:['grass','poison'], height: 0.7}, 
-    {name:'Charizard', type:['fire','flying'], height: 1.7}, 
-    {name:'Squirtle', type:['water'], height: 0.5}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name:'Bulbasaur', type:['grass','poison'], height: 0.7}, 
+        {name:'Charizard', type:['fire','flying'], height: 1.7}, 
+        {name:'Squirtle', type:['water'], height: 0.5}
+    ];
+    
+    function add(pokemon){
+        if(typeof pokemon !== 'object') {
+            alert (typeof pokemon);
+        }else if(Object.keys(pokemon) !== ['name','type','height']){
+          alert('Please include name, type(s), and height.');
+        }else{
+          pokemonList.push(pokemon);
+        }
+    }
+    function getAll(){
+        return pokemonList;
+    } 
+    
+    return {
+        add: add,
+        getAll: getAll
+    }
+})(); 
 
-//Here I intialize global varibles that will be used throughout the script in loops and/or functions
-//    The typeMsg variable will be used to store the types or type of the respective pokemon; to be used in the output message
-//    The i varibale will be used as a counter in loops
-let typeMsg = '';
-let i = 0
 
 //This loop will output a message to the document listing each pokemon along with its height and types.
-for (;i < pokemonList.length; i++) {
+pokemonRepository.getAll().forEach(function(pokemon){
+//Here I intialize global varibles that will be used throughout the script in loops and/or functions
+//    The typeMsg variable will be used to store the types or type of the respective pokemon; to be used in the output message
+    let typeMsg = '';
 //    This if-else statement will be used to form a message listing the pokemon's type based on the number of types.
-    if (pokemonList[i].type.length > 1) {
-        typeMsg = ' has ' + pokemonList[i].type.length + ' types of ' + pokemonList[i].type.toString().replace(',',' and ')+'.';   
+    if (pokemon.type.length > 1) {
+        typeMsg = ' has ' + pokemon.type.length + ' types of ' + pokemon.type.toString().replace(',',' and ')+'.';   
     }else {
-        typeMsg = ' has a type of ' + pokemonList[i].type+'.'; 
+        typeMsg = ' has a type of ' + pokemon.type+'.'; 
     }
 //    Here I store the result, of this ternary operator, in the variable bigPoke to be appended to the output message for pokemon pokemons of 1.5 meters talls.
-    let bigPoke =  pokemonList[i].height > 1.5 ? `- Wow, this is a big pokemon!`:``;
+    let bigPoke =  pokemon.height > 1.5 ? `- Wow, this is a big pokemon!`:``;
 //    Here I form the output message using Template Literal form
-    document.write(`${pokemonList[i].name} is ${pokemonList[i].height} meters tall and  ${typeMsg} ${bigPoke} <br>`);
-}
+    document.write(`${pokemon.name} is ${pokemon.height} meters tall and  ${typeMsg} ${bigPoke} <br>`);
+});
+
+

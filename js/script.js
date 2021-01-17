1be42668d0912f6218084c5f171a0a6c1076cce8
Marquez Moore <marquezmoore23@gmail.com>
2021-01-17 17:09:25 +0100

    Refactored code and add new addListItem function to IIFE

--- js/script.js
+++ js/script.js
@@ -19,30 +19,32 @@ let pokemonRepository = (function() {
     }
     function getAll(){
         return pokemonList;
-    } 
+    }
+    function addListItem(pokemon){
+            let pokeList = document.querySelector('.pokemon-list');
+        
+            let listItem = document.createElement('li');
+            let button = document.createElement('button');
+            button.innerText = pokemon.name;
+            button.classList.add('button-new-style');
+            listItem.appendChild(button);
+            pokeList.appendChild(listItem);
+    }
+   
     
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
     }
 })(); 
 
 
 //This loop will output a message to the document listing each pokemon along with its height and types.
-pokemonRepository.getAll().forEach(function(pokemon){
-//Here I intialize global varibles that will be used throughout the script in loops and/or functions
-//    The typeMsg variable will be used to store the types or type of the respective pokemon; to be used in the output message
-    let typeMsg = '';
-//    This if-else statement will be used to form a message listing the pokemon's type based on the number of types.
-    if (pokemon.type.length > 1) {
-        typeMsg = ' has ' + pokemon.type.length + ' types of ' + pokemon.type.toString().replace(',',' and ')+'.';   
-    }else {
-        typeMsg = ' has a type of ' + pokemon.type+'.'; 
-    }
-//    Here I store the result, of this ternary operator, in the variable bigPoke to be appended to the output message for pokemon pokemons of 1.5 meters talls.
-    let bigPoke =  pokemon.height > 1.5 ? `- Wow, this is a big pokemon!`:``;
-//    Here I form the output message using Template Literal form
-    document.write(`${pokemon.name} is ${pokemon.height} meters tall and  ${typeMsg} ${bigPoke} <br>`);
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
+
+pokemonRepository.getAll().forEach(addListItem(pokemon));
 
 

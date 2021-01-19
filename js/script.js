dab49b8630bf1249fae96053cc43dec9e62f685a
Marquez Moore <marquezmoore23@gmail.com>
2021-01-19 16:09:47 +0100

    Added Functions

--- js/script.js
+++ js/script.js
@@ -20,30 +20,32 @@ let pokemonRepository = (function() {
     function getAll(){
         return pokemonList;
     }
+    // The parameter in the function below is the pokemon object passed through each loop of the for loop calling this function
     function addListItem(pokemon){
             let pokeList = document.querySelector('.pokemon-list');
             let listItem = document.createElement('li');
             let button = document.createElement('button');
             button.innerText = pokemon.name;
-            button.classList.add('button-new-style');
+            // button.classList.add('button-new-style');
             listItem.appendChild(button);
             pokeList.appendChild(listItem);
-            button.addEventListener('click', function(event){
-                let poke = event.target.innerText;
-                alert(`You have selected:  ${poke}`);
+            button.addEventListener('click', function() {
+                showDetails(pokemon);
             })
     }
+    function showDetails(pokemon){
+        let pokeName =  pokemon.name;
+        alert(`You have selected: ${pokeName}`);
+    }
    
-    
+    // The following return will return the functions of this IIFE to the delcared varible to be used outside
+    // this scope/context
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        showDetails: showDetails
     }
 })(); 
 
-
-//This loop will output a message to the document listing each pokemon along with its height and types.
-pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
-});
+pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
